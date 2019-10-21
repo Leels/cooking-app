bd5fee47432bd152774b7536b39dd062943a30b8
Lela Smith and Saul Ordonez <lela+saul@>
2019-10-21 15:10:15 -0700

    add code converting gallons to teaspoons and quarts to liters.

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,14 @@ var ounces = function(grams) {
   return (grams / 28.34952);
 };
 
-// var teaspoons = function(gallon) {
-//   return (gallon * 768);
-// };
-// var gallonsToTeaspoons = teaspoons.toFixed(2);
-//
-// var liter = function(quart) {
-//   return (quarts * 0.946353);
-// };
-// var quartsToLiters = liters.toFixed(2);
+var teaspoons = function(gallon) {
+ return (gallon * 768);
+};
+
+var liter = function(quart) {
+  return (quart * 0.946353);
+};
+
 
 console.log()
 
@@ -24,8 +23,19 @@ $(document).ready(function() {
     var grams = parseFloat($("input#grams").val());
     var result = ounces(grams).toFixed(2);
 
-    $(".grams").text(result);
+    var gallon = parseFloat($("input#gallon").val());
+    var result2 = teaspoons(gallon).toFixed(2);
 
+    var quart = parseFloat($("input#quart").val());
+    var result3 = liter(quart).toFixed(2);
+
+    $(".grams").text(result);
     $("#result").show();
+
+    $(".gallon").text(result2);
+    $("#result2").show();
+
+    $(".quart").text(result3);
+    $("#result3").show();
      });
    });
